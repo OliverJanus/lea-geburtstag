53ae3b6628b527e28270d9e55dd3d11e66f3f24c
Oliver Janus <oliver.janus@googlemail.com>
2026-09-21 20:03:40 +0200

    add test video

--- src/pages/video_page/VideoPage.tsx
+++ src/pages/video_page/VideoPage.tsx
@@ -3,20 +3,22 @@ import "./VideoPage.css";
 function VideoPage() {
   return (
     <div className="video-page">
+      <div className="video-container">
+        <video controls>
+          <source src="test.mp4" type="video/mp4" />
+        </video>
+      </div>
       <div className="loading-overlay">
-        <div className="unlock-icon"></div>
+        <div className="unlock-icon">
+          <img src="checkmark.gif" alt="Unlock Icon" />
+        </div>
         <div className="loading-container">
           <span>Verbindung wird hergestellt...</span>
           <div className="progress-bar">
-            <div className="progress-bar-text">0%</div>
+            <div className="progress-bar-text" />
             <div className="progress-bar-fill" />
           </div>
         </div>
-      </div>
-      <div className="video-container">
-        <video autoPlay muted loop>
-          <source src="/video.mp4" type="video/mp4" />
-        </video>
       </div>
     </div>
   );
